bf60ba194bebda3a6fa52ba8471231608e18d75a
Moritz Johner <moritz.johner@cbc-extern.de>
2020-03-20 20:56:45 +0100

    feat: fetch timeseries from prometheus

--- pkg/server/metrics.go
+++ pkg/server/metrics.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/moolen/statusgraph/pkg/config"
@@ -14,7 +15,12 @@ import (
 type MetricResponse struct {
 	AvailableServices []string `json:"available_services"`
 	// map service_id => query => value
-	Metrics map[string]map[string]string `json:"metrics"` // TODO: support TimeSeries?
+	Metrics map[string]map[string][]TSValue `json:"metrics"`
+}
+
+type TSValue struct {
+	Date  string  `json:"date"`
+	Value float64 `json:"value"`
 }
 
 type QueryResult struct {
@@ -27,7 +33,7 @@ type QueryResult struct {
 
 type Metric struct {
 	Metric map[string]string `json:"metric"`
-	Value  []interface{}     `json:"value"`
+	Value  [][]interface{}   `json:"values"`
 }
 
 func FetchMetrics(cfg *config.ServerConfig) http.HandlerFunc {
@@ -68,7 +74,7 @@ func FetchMetrics(cfg *config.ServerConfig) http.HandlerFunc {
 		}
 
 		for _, query := range cfg.Mapping.MetricConfig.Queries {
-			u, err := url.Parse(fmt.Sprintf("%s/api/v1/query", cfg.Upstream.Prometheus.URL))
+			u, err := url.Parse(fmt.Sprintf("%s/api/v1/query_range", cfg.Upstream.Prometheus.URL))
 			if err != nil {
 				log.Error(err)
 				json.NewEncoder(w).Encode(map[string]bool{"ok": false})
@@ -76,7 +82,11 @@ func FetchMetrics(cfg *config.ServerConfig) http.HandlerFunc {
 			}
 			params := url.Values{}
 			params.Add("query", query.Query)
+			params.Add("start", strconv.Itoa(int(time.Now().Add(-time.Minute*60).Unix())))
+			params.Add("end", strconv.Itoa(int(time.Now().Unix())))
+			params.Add("step", "60")
 			u.RawQuery = params.Encode()
+			log.Infof("q: %s", u)
 			req, err := http.NewRequest(
 				"GET",
 				u.String(),
@@ -104,28 +114,46 @@ func FetchMetrics(cfg *config.ServerConfig) http.HandlerFunc {
 
 			for _, r := range m.Data.Result {
 				if payload.Metrics == nil {
-					payload.Metrics = make(map[string]map[string]string)
+					payload.Metrics = make(map[string]map[string][]TSValue)
 				}
 				svc := r.Metric[query.ServiceLabel]
 				if svc == "" {
 					log.Warnf("query (%s) missing service label (%s)", query.Name, query.ServiceLabel)
 					continue
 				}
-				if len(r.Value) != 2 {
-					log.Warnf("invalid metric value: %#v", r.Value)
-					continue
+
+				for _, v := range r.Value {
+					if len(v) != 2 {
+						log.Warnf("invalid metric value: %#v", v)
+						continue
+					}
+					// 1st: timestamp
+					// 2nd: value
+					d, ok := v[0].(float64)
+					if !ok {
+						log.Warnf("invalid metric value. could not cast to string")
+						continue
+					}
+					val, ok := v[1].(string)
+					if !ok {
+						log.Warnf("invalid metric value. could not cast to string")
+						continue
+					}
+					if payload.Metrics[svc] == nil {
+						payload.Metrics[svc] = make(map[string][]TSValue)
+					}
+					fv, err := strconv.ParseFloat(val, 64)
+					if err != nil {
+						log.Warnf("invalid metric value. could not parse to float")
+						continue
+					}
+					mt := time.Unix(int64(d), 0)
+					payload.Metrics[svc][query.Name] = append(payload.Metrics[svc][query.Name], TSValue{
+						Date:  mt.Format(time.RFC3339),
+						Value: fv,
+					})
 				}
-				// 1st: timestamp
-				// 2nd: value
-				val, ok := r.Value[1].(string)
-				if !ok {
-					log.Warnf("invalid metric value. could not cast to string")
-					continue
-				}
-				if payload.Metrics[svc] == nil {
-					payload.Metrics[svc] = make(map[string]string)
-				}
-				payload.Metrics[svc][query.Name] = val
+
 			}
 		}
 
