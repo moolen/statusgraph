3a0116b85259028fad8cd47aa34f04b4fa540634
Moritz Johner <beller.moritz@googlemail.com>
2020-04-27 20:39:59 +0200

    fix: add favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -106,6 +106,10 @@ module.exports = {
         to: 'index.html',
       },
       {
+        from: './assets/**/',
+        to: '',
+      },
+      {
         from: './**/*.js',
         to: '[name].js',
       },
